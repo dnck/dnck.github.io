3800519caa301ef00f5de0f3f3371763c14d0d52
Dan Cook <cook@Dans-MacBook-Pro.local>
2019-04-16 00:12:38 +0200

    clean up csunitconverter class

--- assets/code/CSUnitConvert.py
+++ assets/code/CSUnitConvert.py
@@ -1,3 +1,8 @@
+# -*- coding: utf-8 -*-
+# A basic converter for cs units
+# useful for gauging download and upload speeds
+
+import argparse
 
 class CSUnitConverter():
     def __init__(self):
@@ -31,15 +36,46 @@ class CSUnitConverter():
         return fsizeBits / uplspeed
 
 if __name__ == "__main__":
-    converter = CSUnitConverter()
-    GB = 1.024e+12
 
-    MSGSIZE =  # Bytes
-    waittime = converter.getDownloadWaitTime(dlspeed=100, fsize=MSGSIZE, fSizeUnitBytes="B")
+
+    GB = 1_000_000_000  # in bytes
+    MB = 1_000_000  # in bytes
+    KB = 1_000  # in bytes
+    MSGSIZE = None # in bytes
+
+    converter = CSUnitConverter()
+
+    parser = argparse.ArgumentParser(description='A very basic units converter for cs apps. Must enter only one file size in Gibabytes, Megabytes, or Kilobytes. If multiple file sizes are entered, the last one entered will be converted only.')
+
+    parser.add_argument('-dlspeed', metavar='Download speed in bps', type=float, default=90, help='Download speed in bits per second')
+
+    parser.add_argument('-ulspeed', metavar='Upload speed in bps', type=float, default=40, help='Upload speed in bits per second')
+
+    parser.add_argument('-gb', metavar='Size of file in GB', type=float, default=None, help='Gibabytes')
+
+    parser.add_argument('-mb', metavar='Size of file in MB', type=float, default=None, help='Megabytes')
+
+    parser.add_argument('-kb', metavar='Size of file in KB', type=float, default=None, help='Kilobytes')
+
+    args = parser.parse_args()
+    dlspeed = args.dlspeed
+    ulspeed = args.ulspeed
+
+    # test condition 
+    assert any([args.gb, args.mb, args.kb]), "Must supply file size in GB, MB, or KBs"
+
+    if args.gb:
+        MSGSIZE = args.gb * GB
+    if args.mb:
+        MSGSIZE = args.mb * MB
+    if args.kb:
+        MSGSIZE = args.kb * KB
+
+    waittime = converter.getDownloadWaitTime(dlspeed=dlspeed, fsize=MSGSIZE, fSizeUnitBytes="B")
 
     print("The download will take approx {} seconds.".format(waittime))
 
-    waittime = converter.getUploadWaitTime(uplspeed=10, fsize=100, fSizeUnitBytes="B")
+    waittime = converter.getUploadWaitTime(uplspeed=ulspeed, fsize=MSGSIZE, fSizeUnitBytes="B")
     print("The upload will take approx {} seconds.".format(waittime))
 
-    print("How long will it take a file of size {}{} to saturate {} of the network nodes?".format(1024,"B","80%"))
+    #print("How long will it take a file of size {}{} to saturate {} of the network nodes?".format(1024,"B","80%"))
--- assets/code/frontpage.js
+++
@@ -1,4 +0,0 @@
-var svg = d3.select("svg");
-var circle = svg.selectAll("circle")
-circle.style("fill", "steelblue");
-console.log(circle.fill)
