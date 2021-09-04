1e5f3927181a3d820fbb755ca3b29d8d31b87346
Dan John Cook <dco@innovation-rocks.com>
2021-09-04 15:28:51 +0200

    Line length

--- tools/dns-tool/run.sh
+++ tools/dns-tool/run.sh
@@ -4,10 +4,10 @@
 # as a background process and dump the log to server.log.
 # If, instead you want to run the Docker image, then use,
 # ```
-# make build && \
-#  sudo docker run -d --dns 127.0.0.1 --name dns-tool dnck.github.io/dns-tool && \
-#    sudo docker exec -it dns-tool dig +tcp www.danjcook.com && \
-#     sudo docker logs dns-tool
+# make build &&\
+# sudo docker run -d --dns 127.0.0.1 --name dns-tool dnck.github.io/dns-tool &&\
+# sudo docker exec -it dns-tool dig +tcp www.danjcook.com &&\
+# sudo docker logs dns-tool
 # ```
 # Thanks!
 # - Daniel Cook
